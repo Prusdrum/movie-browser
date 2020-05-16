ef8391ad82144d0d9850aeea3f033f2f1032f012
Mateusz Prusaczyk <mateusz.prusaczyk@codete.com>
2020-05-16 13:07:03 +0200

    Updates

---
+++ precache-manifest.eedfb1037dae52d5758a88b00fdee9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9416c9abf03e4c6eb1a14cc69548a0b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea37b3fb5c4a007f6de8",
+    "url": "/static/css/main.c5b05437.chunk.css"
+  },
+  {
+    "revision": "6a68cd8efbac1247a7f9",
+    "url": "/static/js/2.c672b9d5.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.c672b9d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea37b3fb5c4a007f6de8",
+    "url": "/static/js/main.25c6b55c.chunk.js"
+  },
+  {
+    "revision": "415573b2210a83cc54ec",
+    "url": "/static/js/runtime-main.b949331a.js"
+  }
+]);
