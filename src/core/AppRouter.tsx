f37b753a20fcbe98c52738de301cb11c867e7e4b
Mateusz Prusaczyk <mateusz.prusaczyk@codete.com>
2020-05-16 13:10:57 +0200

    added basename to the router. added predeploy script

--- src/core/AppRouter.tsx
+++ src/core/AppRouter.tsx
@@ -23,7 +23,7 @@ export default () => {
   }
 
   return (
-    <Router>
+    <Router basename={"/movie-browser"}>
       <Switch>
         <Route exact path={definition.home}>
           <Redirect to={path.goToSearchPage()}/>
